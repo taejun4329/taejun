354ef876a558a0a3a37016cac25f81a5abab769c
taejun <taejun4629@naver.com>
2022-03-10 09:58:39 +0900

    [JAVA] Hello.java 생성
    hello.world를 출력하는 코드 작성

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * 첫 번째 예제 프로그램
+ * @version 1.0 2022-03-10
+ * author 박태준
+ */
+public class Hello {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello.world!"); // "Hello.world!"를 출력한다.
+		System.out.println("I'm a new java programmer.");
+	}
+
+}
