221466f68f50889105731e43fead355d222be2df
taejun <taejun4629@naver.com>
2022-03-10 10:33:24 +0900

    Pi 값 상수로 변경.

---
+++ src/Areatest.java
@@ -0,0 +1,14 @@
+
+public class Areatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 3.0;
+		final double Pi = 3.141592;
+		double area = Pi * radius * radius;
+		
+		System.out.println("반지름이"+radius+"인 원의 면적은 "+area+"입니다.");
+
+	}
+
+}
